4594784b247b020f64f4329bd3117890c9d1a625
rpasiutarchitech <rpasiut@architech.ca>
2020-07-03 13:27:15 +0200

    schematics for generating complex modules

--- create-complex-module/src/create-complex-module/index.ts
+++ create-complex-module/src/create-complex-module/index.ts
@@ -1,10 +1,38 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  apply,
+  applyTemplates,
+  chain,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
+function generateFiles(schema: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const templateSource = apply(url('./files'), [
+      applyTemplates({
+        classify: strings.classify,
+        dasherize: strings.dasherize,
+        name: schema.name
+      }),
+      move('src/app/modules')
+    ]);
+
+    return chain([mergeWith(templateSource)])(tree, context);
+  };
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function createComplexModule(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function createComplexModule(schema: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    return chain([generateFiles(schema)])(
+      tree,
+      context
+    );
   };
 }
